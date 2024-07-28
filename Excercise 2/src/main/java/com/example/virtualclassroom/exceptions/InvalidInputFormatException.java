8ece13588095ea187a148532a7e87e353433e524
Sibhiraj-2004 <sibhiraj2004@gmail.com>
2024-07-28 23:56:59 +0530

    Add files via upload

---
+++ Excercise 2/src/main/java/com/example/virtualclassroom/exceptions/InvalidInputFormatException.java
@@ -0,0 +1,7 @@
+package main.java.com.example.virtualclassroom.exceptions;
+
+public class InvalidInputFormatException extends Exception {
+    public InvalidInputFormatException(String message) {
+        super(message);
+    }
+}
